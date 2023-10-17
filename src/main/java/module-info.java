268287d46706aac8eb5da60622ea446df81cf234
saydullayev-d <saydullayevdamir03@gmail.com>
2023-10-17 09:32:23 +0500

    add delete change

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.sport_project_with_db {
     requires java.sql;
 
 
+
     opens com.example.sport_project_with_db to javafx.fxml;
     exports com.example.sport_project_with_db;
     exports com.example.sport_project_with_db.db_actions;
